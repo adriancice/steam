442bb235f83259a0b65e6ac3f89f31242e9a1270
Adrian <naikonpaul@gmail.com>
2021-01-27 00:13:54 +0100

    summaries

--- force-app/main/default/lwc/stats/stats.js
+++ force-app/main/default/lwc/stats/stats.js
@@ -1,11 +1,23 @@
 import { LightningElement } from 'lwc';
+import fetchPlayerSummaries from '@salesforce/apex/Steam_Controller.fetchPlayerSummaries';
 
 export default class Stats extends LightningElement {
-    queryTerm;
+    summaries = false;
+    personName;
+    avatarMedium;
     handleKeyUp(evt) {
         const isEnterKey = evt.keyCode === 13;
         if (isEnterKey) {
             this.queryTerm = evt.target.value;
+            fetchPlayerSummaries({ steamId: this.queryTerm }).then(result => {
+                const response = JSON.parse(result);
+                console.log(JSON.stringify(response));
+                this.personName = response.personaname;
+                this.avatarMedium = response.avatarmedium;
+                this.summaries = true;
+            }).catch(error => {
+                console.log('Error: ');
+            });
         }
     }
 }
