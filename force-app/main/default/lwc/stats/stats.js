0dd5fe51f9c417f246d51af2423b70cb5ace9a3e
Adrian <naikonpaul@gmail.com>
2021-02-02 00:32:35 +0100

    auth metadata

--- force-app/main/default/lwc/stats/stats.js
+++ force-app/main/default/lwc/stats/stats.js
@@ -1,23 +1,43 @@
-import { LightningElement } from 'lwc';
-import fetchPlayerSummaries from '@salesforce/apex/Steam_Controller.fetchPlayerSummaries';
+import { LightningElement } from "lwc";
+import fetchPlayerSummaries from "@salesforce/apex/Steam_Controller.fetchPlayerSummaries";
 
 export default class Stats extends LightningElement {
-    summaries = false;
-    personName;
-    avatarMedium;
-    handleKeyUp(evt) {
-        const isEnterKey = evt.keyCode === 13;
-        if (isEnterKey) {
-            this.queryTerm = evt.target.value;
-            fetchPlayerSummaries({ steamId: this.queryTerm }).then(result => {
-                const response = JSON.parse(result);
-                console.log(JSON.stringify(response));
-                this.personName = response.personaname;
-                this.avatarMedium = response.avatarmedium;
-                this.summaries = true;
-            }).catch(error => {
-                console.log('Error: ');
-            });
-        }
+  summaries = false;
+  personName;
+  avatarMedium;
+  steamId;
+  handleKeyUp(evt) {
+    const isEnterKey = evt.keyCode === 13;
+    if (isEnterKey) {
+      this.queryTerm = evt.target.value;
+      fetchPlayerSummaries({ steamId: this.queryTerm })
+        .then((result) => {
+          const response = JSON.parse(result);
+          console.log(JSON.stringify(response));
+          this.personName = response.personaname;
+          this.avatarMedium = response.avatarmedium;
+          this.summaries = true;
+        })
+        .catch((error) => {
+          console.log("Error: " + JSON.stringify(error));
+        });
     }
+  }
+  changeSteamId(event) {
+    console.log("busco::: " + event.target.value);
+    this.steamId = event.target.value;
+  }
+  handleClick() {
+    fetchPlayerSummaries({ steamId: this.steamId })
+      .then((result) => {
+        const response = JSON.parse(result);
+        console.log(JSON.stringify(response));
+        this.personName = response.personaname;
+        this.avatarMedium = response.avatarmedium;
+        this.summaries = true;
+      })
+      .catch((error) => {
+        console.log("Error: " + JSON.stringify(error));
+      });
+  }
 }
